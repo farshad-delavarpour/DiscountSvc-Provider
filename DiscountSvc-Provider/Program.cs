17c0df04def11be0db14c1a7cae101feef29f8c5
Farshad <farshaddelavarpour@gmail.com>
2023-03-25 17:23:05 +0430

    Add Discount Model and Service - Add Service to program.cs

--- DiscountSvc-Provider/Program.cs
+++ DiscountSvc-Provider/Program.cs
@@ -1,25 +1,27 @@
+using DiscountSvc_Provider.Models;
+using DiscountSvc_Provider.Services;
+
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddTransient<DiscountService>();
 
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
 app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
 app.MapControllers();
-
+app.UseRouting();
+app.UseAuthorization();
+app.UseEndpoints(endpoints =>
+{
+    var svc = endpoints
+        .ServiceProvider
+        .GetRequiredService<DiscountService>();
+    endpoints.MapPost("/discount", async context =>
+    {
+        var model = await context.Request.ReadFromJsonAsync<DiscountModel>();
+        var amount = svc.GetDiscountAmount(model.CustomerRating);
+        await context.Response.WriteAsJsonAsync(new DiscountModel { CustomerRating = model.CustomerRating, AmountToDiscount = amount });
+    });
+});
 app.Run();
